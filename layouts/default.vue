a5980eb0cd38be102d5b0fd4eb79bda0fa33f67b
Wilson Gouanet <wilstito@gmail.com>
2020-02-16 14:03:34 +0100

    initialize pages and layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,5 @@
 <template>
   <v-app>
-    <v-toolbar color="deep-purple accent-4" dense dark style="flex: 0;">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vue Note App</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on }">
-          <v-btn v-on="on" icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-toolbar>
-    <v-content>
-      <v-flex class="text-center">
-        <img src="/v.png" alt="Vuetify.js" class="mt-2" />
-        <blockquote class="blockquote">
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-flex>
-    </v-content>
+    <nuxt />
   </v-app>
 </template>
