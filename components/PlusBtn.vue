e33b0aa0f04aec2261ef75787b9f74548d68e145
Wilson Gouanet <wilstito@gmail.com>
2020-02-16 20:52:51 +0100

    create plusBtn component

---
+++ components/PlusBtn.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-btn absolute dark fab color="primary" class="plus-btn">
+    <v-icon>add</v-icon>
+  </v-btn>
+</template>
+
+<style scoped>
+.plus-btn {
+  bottom: 16px;
+  right: 0px;
+}
+</style>
