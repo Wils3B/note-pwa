d34228f59efb7586fba7bd2fc14af223a69b39a5
Wilson Gouanet <wilstito@gmail.com>
2020-02-18 18:50:11 +0100

    create search page

---
+++ pages/search.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="v-application--wrap search">
+    <v-app-bar color="primary" dark fixed>
+      <v-btn @click="$router.go(-1)" icon class="mr-4">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+
+      <v-text-field
+        hide-details
+        prepend-icon="search"
+        single-line
+        autofocus
+        clearable
+        type="search"
+        placeholder="Search here"
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>clear</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      App search page.
+      <app-plus-btn />
+    </v-content>
+  </div>
+</template>
+
+<style lang="scss">
+.search {
+  .v-toolbar {
+    flex-grow: 0;
+  }
+  .v-content {
+    background-color: $light-grey;
+    padding: 72px 16px 0px 16px !important;
+  }
+}
+</style>
+
+<script>
+import AppPlusBtn from '~/components/PlusBtn.vue'
+
+export default {
+  components: {
+    AppPlusBtn
+  }
+}
+</script>
