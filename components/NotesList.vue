e900e5a57b514a74dd1b4257a6bef0a588db7c1d
Wilson Gouanet <wilstito@gmail.com>
2020-02-19 23:00:48 +0100

    add note list component

---
+++ components/NotesList.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row>
+    <v-col v-for="(n, index) in notes" :key="index" cols="6" lg="2">
+      <app-note-card :note="n" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import AppNoteCard from '~/components/NoteCard.vue'
+
+export default {
+  components: {
+    AppNoteCard
+  },
+  props: {
+    notes: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
