22741945ea83c85f92c18347d6089e6e609cc929
Wilson Gouanet <wilstito@gmail.com>
2020-02-19 23:00:29 +0100

    add note card component

---
+++ components/NoteCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="note-card">
+    <v-img
+      v-if="note.hasImage()"
+      :src="note.getFirstImage()"
+      height="64px"
+    ></v-img>
+    <v-card-text>
+      {{ note.content }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
