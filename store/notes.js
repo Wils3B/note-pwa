5d7b29de155e42a003bc33025225ed03f4d2740a
Wilson Gouanet <wilstito@gmail.com>
2020-02-21 21:02:18 +0100

    add save note mutation

--- store/notes.js
+++ store/notes.js
@@ -3,10 +3,24 @@ import Note from '~/model/note'
 export const state = () => {
   return {
     notes: [
-      new Note('Note 1'),
-      new Note('Note 2 of 2002'),
-      new Note('Note 3 is the best note of my store'),
-      new Note('In this great future, where is the very')
+      new Note(
+        'Trademarked names, logos, and images may appear in this book. Rather than use a trademark symbol with every occurrence of a trademarked name, logo, or image we use the names, logos, and images only in an editorial fashion and to the benefit of the trademark owner, with no intention of infringement of the trademark.'
+      ),
+      new Note(
+        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
+      ),
+      new Note(
+        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
+      ),
+      new Note(
+        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
+      )
     ]
+  }
+}
+
+export const mutations = {
+  saveNote(currentState, note) {
+    currentState.notes.push(note)
   }
 }
