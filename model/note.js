da131d977fd05f7702403fc52b9d101e14b3e167
Wilson Gouanet <wilstito@gmail.com>
2020-02-21 22:18:04 +0100

    improve images methods and set modifiedAt prop

--- model/note.js
+++ model/note.js
@@ -1,14 +1,14 @@
 export default class Note {
   constructor(content = '') {
     this.createdAt = new Date().toISOString()
-    this.modifiedAt = this.createAt
+    this.modifiedAt = this.createdAt
     this.content = content
     this.voices = []
     this.color = 'white'
   }
 
   hasImage() {
-    return /<img src=".+" \/>/.test(this.content)
+    return /<img src=".+">/.test(this.content)
   }
 
   hasVoices() {
@@ -16,9 +16,9 @@ export default class Note {
   }
 
   getFirstImage() {
-    const match = this.content.match(/<img src=".+" \/>/)
+    const match = this.content.match(/<img src=".+">/)
     if (match) {
-      return match[0].match(/(?<=src=").+(?=" \/>)/)
+      return match[0].match(/(?<=src=").+(?=">)/)[0]
     }
     return null
   }
