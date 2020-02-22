3a457cc1a2ba24fa99835dcfd2bb3c6b1a51f5f3
Wilson Gouanet <wilstito@gmail.com>
2020-02-22 17:31:23 +0100

    add note details page

---
+++ pages/note-detail/_id.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="v-application--wrap details">
+    <v-app-bar color="primary" dark fixed>
+      <v-btn @click="$router.go(-1)" icon>
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+      <v-toolbar-title>
+        {{ modifiedDate }}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>edit</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <div v-html="note.content"></div>
+    </v-content>
+  </div>
+</template>
+
+<script>
+import Note from '~/model/note'
+
+export default {
+  data() {
+    return {
+      note: new Note()
+    }
+  },
+  computed: {
+    modifiedDate() {
+      const date = new Date(this.note.modifiedAt)
+      const now = new Date()
+      if (
+        date.getUTCDay() === now.getUTCDay() &&
+        date.getUTCMonth() === now.getUTCMonth() &&
+        date.getUTCFullYear() === now.getUTCFullYear()
+      ) {
+        return date.toTimeString().slice(0, 5)
+      }
+      return this.note.modifiedAt.slice(0, 10)
+    }
+  },
+  mounted() {
+    this.note = this.$store.getters['notes/noteById'](
+      Number(this.$route.params.id)
+    )[0]
+  }
+}
+</script>
+
+<style lang="scss">
+.details {
+  .v-toolbar {
+    flex-grow: 0;
+  }
+  .v-content {
+    background-color: $light-grey;
+    padding: 72px 16px 0px 16px !important;
+  }
+}
+</style>
