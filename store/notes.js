775d4d52463e1aefb2d31f18f758d5ad932cc140
Wilson Gouanet <wilstito@gmail.com>
2020-02-22 17:31:57 +0100

    add getter for note by id

--- store/notes.js
+++ store/notes.js
@@ -1,26 +1,18 @@
-import Note from '~/model/note'
-
 export const state = () => {
   return {
-    notes: [
-      new Note(
-        'Trademarked names, logos, and images may appear in this book. Rather than use a trademark symbol with every occurrence of a trademarked name, logo, or image we use the names, logos, and images only in an editorial fashion and to the benefit of the trademark owner, with no intention of infringement of the trademark.'
-      ),
-      new Note(
-        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
-      ),
-      new Note(
-        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
-      ),
-      new Note(
-        'This work is subject to copyright. All rights are reserved by the Publisher, whether the whole or part of the material is concerned, specifically the rights of translation, reprinting, reuse of illustrations, recitation, broadcasting, reproduction on microfilms or in any other physical way, and transmission or information storage and retrieval, electronic adaptation, computer software, or by similar or dissimilar methodology now known or hereafter developed.'
-      )
-    ]
+    notes: []
   }
 }
 
 export const mutations = {
   saveNote(currentState, note) {
     currentState.notes.push(note)
+  }
+}
+
+export const getters = {
+  noteById(currentState) {
+    window.notes = currentState.notes
+    return (id) => currentState.notes.filter((note) => note.id === id)
   }
 }
