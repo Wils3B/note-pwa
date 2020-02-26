3600fc806f14a3b8471b1a229ffefa14eec749d0
Wilson Gouanet <wilstito@gmail.com>
2020-02-26 12:20:44 +0100

    add communication with localstorage to notes store

--- store/notes.js
+++ store/notes.js
@@ -1,17 +1,28 @@
+import Note from '~/model/note'
+
 export const state = () => {
+  const notes = []
+  for (let i = 0; i < localStorage.length; i += 1) {
+    const key = localStorage.key(i)
+    if (/^note-\d{1,}$/.test(key)) {
+      notes.push(Note.fromJSON(localStorage.getItem(key)))
+    }
+  }
   return {
-    notes: []
+    notes
   }
 }
 
 export const mutations = {
   saveNote(currentState, note) {
     currentState.notes.push(note)
+    localStorage.setItem(`note-${note.id}`, JSON.stringify(note))
   },
   deleteNote(currentState, id) {
     const note = currentState.notes.filter((note) => note.id === id)[0]
     const index = currentState.notes.indexOf(note)
     currentState.notes.splice(index, 1)
+    localStorage.removeItem(`note-${id}`)
   },
   editNote(currentState, note) {
     const index = currentState.notes.indexOf(
@@ -22,6 +33,7 @@ export const mutations = {
     currentNote.setContent(note.content)
     currentState.notes.splice(index, 1)
     currentState.notes.push(currentNote)
+    localStorage.setItem(`note-${currentNote.id}`, JSON.stringify(currentNote))
   }
 }
 
