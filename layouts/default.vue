eed4c1cb1afe6d97b5755b750b24b4c10a993de9
Wilson Gouanet <wilstito@gmail.com>
2020-02-27 18:58:03 +0100

    add basic transition for pages switching

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,4 +6,14 @@
 
 <style lang="scss">
 @import '~vuetify/src/styles/main.sass';
+
+.page-enter-active,
+.page-leave-active {
+  transition: transform 0.4s;
+  transform-origin: bottom right;
+}
+.page-enter,
+.page-leave-active {
+  transform: translateX(-100%);
+}
 </style>
