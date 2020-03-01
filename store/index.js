8d28b61caa40bb4541bb1034fb19dcd504569846
Wilson Gouanet <wilstito@gmail.com>
2020-03-01 20:19:28 +0100

    add db to root state of vueX stores

--- store/index.js
+++ store/index.js
@@ -1,5 +1,8 @@
+import DB from '~/model/db'
+
 export const state = () => {
   return {
-    colors: ['white', 'yellow', 'purple', 'green', 'red', 'blue', 'brown']
+    colors: ['white', 'yellow', 'purple', 'green', 'red', 'blue', 'brown'],
+    db: new DB()
   }
 }
