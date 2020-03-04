d11d572e6cd2a8633649fbe7844d7c8cfc077802
Wilson Gouanet <wilstito@gmail.com>
2020-03-04 09:56:48 +0100

    make grid column as setable props

--- components/NotesList.vue
+++ components/NotesList.vue
@@ -1,14 +1,6 @@
 <template>
   <v-row class="note-list">
-    <v-col
-      v-for="n in notes"
-      :key="n.id"
-      cols="6"
-      sm="4"
-      md="3"
-      lg="2"
-      class="note-wrapper"
-    >
+    <v-col v-for="n in notes" :key="n.id" v-bind="sizes" class="note-wrapper">
       <app-note-card :note="n" />
     </v-col>
   </v-row>
@@ -26,6 +18,15 @@ export default {
     notes: {
       type: Array,
       default: () => []
+    },
+    sizes: {
+      type: Object,
+      default: () => ({
+        cols: 6,
+        sm: 4,
+        md: 3,
+        lg: 2
+      })
     }
   }
 }
