973c94b5b874cf923c2549ddf5d849d5ff6ec46e
Wilson Gouanet <wilstito@gmail.com>
2020-03-09 21:02:11 +0100

    removes notes from test file and expose it

---
+++ tests/notes.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    createdAt: '2020-03-03T20:24:52.854Z',
+    modifiedAt: '2020-03-03T20:24:52.854Z',
+    content: 'note 1583267092854',
+    voices: [],
+    color: 'white',
+    id: 1583267092854
+  },
+  {
+    createdAt: '2020-03-03T20:25:33.077Z',
+    modifiedAt: '2020-03-03T20:25:33.077Z',
+    content: 'note 1583267133077',
+    voices: [],
+    color: 'purple',
+    id: 1583267133077
+  },
+  {
+    createdAt: '2020-03-03T20:25:41.644Z',
+    modifiedAt: '2020-03-03T20:25:41.644Z',
+    content: 'note 1583267075908',
+    voices: [],
+    color: 'red',
+    id: 1583267075908
+  },
+  {
+    createdAt: '2020-03-03T20:27:18.614Z',
+    modifiedAt: '2020-03-03T20:27:18.614Z',
+    content: 'note 1583266765730',
+    voices: [],
+    color: 'green',
+    id: 1583266765730
+  },
+  {
+    createdAt: '2020-03-03T20:38:16.948Z',
+    modifiedAt: '2020-03-03T20:38:16.948Z',
+    content: 'note 1583267808194',
+    voices: [],
+    color: 'yellow',
+    id: 1583267808194
+  },
+  {
+    createdAt: '2020-03-04T08:42:40.827Z',
+    modifiedAt: '2020-03-04T08:42:40.827Z',
+    content: 'note 1583311360827',
+    voices: [],
+    color: 'white',
+    id: 1583311360827
+  },
+  {
+    createdAt: '2020-03-04T08:49:54.631Z',
+    modifiedAt: '2020-03-04T08:49:54.631Z',
+    content: 'note 1583311794631',
+    voices: [],
+    color: 'yellow',
+    id: 1583311794631
+  },
+  {
+    createdAt: '2020-03-04T09:17:13.510Z',
+    modifiedAt: '2020-03-04T09:17:13.510Z',
+    content: 'note 1583313433510',
+    voices: [],
+    color: 'yellow',
+    id: 1583313433510
+  },
+  {
+    createdAt: '2020-03-06T06:00:32.437Z',
+    modifiedAt: '2020-03-06T06:00:32.437Z',
+    content: 'note 1583474432437',
+    voices: [],
+    color: 'green',
+    id: 1583474432437
+  },
+  {
+    createdAt: '2020-03-06T07:06:49.029Z',
+    modifiedAt: '2020-03-06T07:06:49.029Z',
+    content: 'note 1583478409029',
+    voices: [],
+    color: 'blue',
+    id: 1583478409029
+  },
+  {
+    createdAt: '2020-03-06T07:10:44.306Z',
+    modifiedAt: '2020-03-06T07:10:44.306Z',
+    content: 'note 1583478644306',
+    voices: [],
+    color: 'yellow',
+    id: 1583478644306
+  },
+  {
+    createdAt: '2020-03-08T07:39:42.241Z',
+    modifiedAt: '2020-03-08T07:39:42.241Z',
+    content: 'note 1583311559736',
+    voices: [],
+    color: 'blue',
+    id: 1583311559736
+  }
+]
