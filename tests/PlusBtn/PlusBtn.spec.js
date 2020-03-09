e9a0c0325e987fecbdaffe18d7d6b28a77780a50
Wilson Gouanet <wilstito@gmail.com>
2020-03-09 10:10:09 +0100

    create tests for plus button component

---
+++ tests/PlusBtn/PlusBtn.spec.js
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import { localInstance } from '../common'
+import router from '../router'
+import PlusBtn from '~/components/PlusBtn.vue'
+
+const localVue = localInstance()
+
+describe('Plus Button', () => {
+  const wrapper = mount(PlusBtn, {
+    localVue,
+    router
+  })
+
+  test('it should be a vue instance named PlusButton', () => {
+    expect(wrapper.isVueInstance).toBeTruthy()
+  })
+
+  test('Have a correct render', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  })
+
+  test('it should have correct icon', () => {
+    expect(wrapper.find('i.v-icon').text()).toBe('add')
+  })
+
+  test('it should be a link to edit new note', () => {
+    expect(wrapper.attributes('href')).toBe('#/edit-note/new')
+  })
+})
