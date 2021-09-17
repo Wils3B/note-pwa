75cf7edfa5fea2f71a48b4e550f82556d575f120
Wilson Gouanet <wilstito@gmail.com>
2021-09-17 09:04:51 -0400

    remove pages transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,16 +13,6 @@ export default {
 </script>
 
 <style lang="scss">
-.page-enter-active,
-.page-leave-active {
-  transition: transform 0.5s;
-  transform-origin: top right;
-}
-.page-enter,
-.page-leave-active {
-  transform: rotateZ(90deg) scale(0.1);
-}
-
 #app {
   background-color: #673ab7;
   background-image: url('/images/icons/icon-144x144.png');
